9f432a1174ab421eaf2fdb067a185f337b80cd76
Kirill21661 <60366365+Kirill21661@users.noreply.github.com>
2022-02-13 16:13:04 +0300

    tiktok

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
 import React, {useState} from 'react'
+import photo from './assets/1.png';
+import photo2 from './assets/2.png';
+import photo3 from './assets/3.png';
+import photo4 from './assets/4.png';
+import photo5 from './assets/5.png';
+import photo6 from './assets/6.png';
+import photo7 from './assets/7.png';
+import photo8 from './assets/8.png';
+import photo9 from './assets/9.png';
+import photo10 from './assets/10.png';
+import photo11 from './assets/11.png';
+import photo12 from './assets/12.png';
+import photo13 from './assets/13.png';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,25 +22,75 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <center><h1>Road to 100000000 clicks<br />
-      Идём до 10000000 кликов</h1></center>
-      <center>{count} click/кликов</center> 
-      <center><button onClick={() => setCount(count + 1)}>+</button></center>
-      <center><button onClick={() => setCount(count - 1)}>-</button></center>
-      <center><button onClick={() => setCount(count + 100)}>+ 100</button></center>
-      <center><button onClick={() => setCount(count - 100)}>- 100</button></center>
-      <center><button onClick={() => setCount(count + 1000)}>+ 1000</button></center>
-      <center><button onClick={() => setCount(count - 1000)}>- 1000</button></center>
-      <center><button onClick={() => setCount(count + 10000)}>+ 10000</button></center>
-      <center><button onClick={() => setCount(count - 10000)}>- 10000</button></center>
-      <center><button onClick={() => setCount(count + 100000)}>+ 100000</button></center>
-      <center><button onClick={() => setCount(count - 100000)}>- 100000</button></center>
-      <center><button onClick={() => setCount(count + 1000000)}>+ 1000000</button></center>
-      <center><button onClick={() => setCount(count - 1000000)}>- 1000000</button></center>
-      <center><button onClick={() => setCount(count + randomInteger(1, 100000000))}>+ СЛУЧАЙНОЕ ЧИСЛО</button></center>
-      <center><button onClick={() => setCount(count - randomInteger(1, 100000000))}>- СЛУЧАЙНОЕ ЧИСЛО</button></center>
-      <center><button onClick={() => setCount(0)}>ОБНУЛИТЬ СЧЁТЧИК</button></center>
+    <div className="container">
+      <div class="text">Road to 1000000000 clicks</div>
+      <div class="text">Дорога до 1000000000 кликов</div>
+      <div class="count">{count}</div>
+      <div className="button__content">
+        <button className='button__content-btn' onClick={() => setCount(count + 100)}>
+          <img class="button__contnet-img" src={photo}/>
+          <div class="button__content-text">+ 100</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count + 1000)}>
+          <img class="button__contnet-img" src={photo3}/>
+          <div class="button__content-text">+ 1000</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count + 10000)}>
+          <img class="button__contnet-img" src={photo5}/>
+          <div class="button__content-text">+ 10000</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count + 100000)}>
+          <img class="button__contnet-img" src={photo7}/>
+          <div class="button__content-text">+ 100000</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count + 1000000)}>
+          <img class="button__contnet-img" src={photo9}/>
+          <div class="button__content-text">+ 1000000</div>
+        </button>
+      </div> 
+      <div className="button__content">
+        <button className='button__content-btn' onClick={() => setCount(count - 100)}>
+        <img class="button__contnet-img" src={photo2}/>
+          <div class="button__content-text">- 100</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count - 1000)}>
+          <img class="button__contnet-img" src={photo4}/>
+          <div class="button__content-text">- 1000</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count - 10000)}>
+          <img class="button__contnet-img" src={photo6}/>
+          <div class="button__content-text">- 10000</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count - 100000)}>
+          <img class="button__contnet-img" src={photo8}/>
+          <div class="button__content-text">- 100000</div>
+        </button>
+        <button className='button__content-btn' onClick={() => setCount(count - 1000000)}>
+          <img class="button__contnet-img" src={photo13}/>
+          <div class="button__content-text">- 1000000</div>
+        </button>
+      </div> 
+
+
+      <div className='button__random'>
+        <button className='button__random-btn' onClick={() => setCount(count + randomInteger(1, 10000000))}>
+          <img class="button__contnet-img" src={photo11}/>
+          <div class="button__content-text">+ СЛУЧАЙНОЕ ЧИСЛО</div>
+        </button>
+        <button className='button__random-btn' onClick={() => setCount(count - randomInteger(1, 10000000))}>
+          <img class="button__contnet-img" src={photo10}/>
+          <div class="button__content-text">- СЛУЧАЙНОЕ ЧИСЛО</div>
+        </button>
+        <button className='button__random-btn' onClick={() => setCount(0)}>
+          <img class="button__contnet-img" src={photo12}/>
+          <div class="button__content-text">ОБНУЛИТЬ СЧЁТЧИК</div>
+        </button>
+      </div>
+
+      <div className="button_plus-minus">
+        <button className="button_plus-minus--btn" onClick={() => setCount(count + 1)}>+</button>
+        <button className="button_plus-minus--btn" onClick={() => setCount(count - 1)}>-</button>
+      </div>
     </div>
   );
 }
